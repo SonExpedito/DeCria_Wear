d53806ad65e470edd6206d29059acf4c73e4571b
SonExpedito <alexexpeditoff7@gmail.com>
2025-06-04 12:36:31 -0300

    NativeWind Config

---
+++ DeCRIA/src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {View, Text} from "react-native"
+import "../../global.css";
+
+
+export default function Index(){
+    return(
+        <View>
+                <Text>Alex Expedito</Text>
+            
+        </View>
+
+    )
+
+}
